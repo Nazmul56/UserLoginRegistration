90a9ea988f987e92f7e0c5be04dd5b5467d4b7ee
nazmul56 <nazmul.cste07@gmail.com>
2016-12-12 22:21:41 +0600

    First Commit

---
+++ LoginRegistrationPHP/dbConnect.php
@@ -0,0 +1,8 @@
+<?php
+ define('HOST','mysql12.000webhost.com');
+ define('USER','a8116690_nazmul');
+ define('PASS','ash1201056m');
+ define('DB','a8116690_gcm');
+ 
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+ ?>
